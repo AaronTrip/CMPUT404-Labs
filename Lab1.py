c83c5024ced5e95cea7b0544ade496a8bac337c4
Student <atripodi@ualberta.ca>
2021-09-07 19:03:57 -0400

    maybe last commit

--- Lab1.py
+++ Lab1.py
@@ -10,6 +10,17 @@ print()
 
 
 google = requests.get("https://www.google.com")
-print(google.text)
+#print(google.text)
 
 
+url = "https://raw.githubusercontent.com/AaronTrip/CMPUT404-Labs/main/Lab1.py"
+
+urlfile = requests.get(url, allow_redirects=True)
+
+
+print()
+print("Here is the files source code")
+print()
+
+
+print(urlfile.content)
